c350c80dc645e577a47db7d60170294fe16e09c7
Udit_Jain <82581308+uditjain-noob@users.noreply.github.com>
2022-04-16 16:51:38 +0530

    Add files via upload

---
+++ CustomerDataEntry.sql
@@ -0,0 +1,4 @@
+INSERT INTO T4_Customer VALUES
+('100001', 'John', 'Cusack', '1990-01-01', 'M', 'Maharashtra, India', 943204482, 'johncusack@gmail.com', 'J00001', 'Married', 00001),
+('100002', 'Octavia', 'Blake', '1991-01-02', 'F', 'Orissa, India', 981584022, 'octaviabla@gmail.com', 'R00002', 'Single', 00002);
+
---
+++ SELECTSTATEMENTS.sql
@@ -0,0 +1,23 @@
+-- SELECT STATEMENTS TO VIEW DATA
+
+USE T4_Car_Insurance_DB;
+
+SELECT * FROM T4_Customer;
+SELECT * FROM T4_Application;
+SELECT * FROM T4_Quote;
+SELECT * FROM T4_Premium_Payment;
+SELECT * FROM T4_Claim;
+SELECT * FROM T4_Claim_Settlement;
+SELECT * FROM T4_Coverage;
+SELECT * FROM T4_Insurance_policy;
+SELECT * FROM T4_Vehicle;
+SELECT * FROM T4_Department;
+SELECT * FROM T4_Office;
+SELECT * FROM T4_Vehicle_service;
+SELECT * FROM T4_Product;
+SELECT * FROM T4_Insurance_company;
+SELECT * FROM T4_Receipt;
+SELECT * FROM T4_Insurance_Policy_Coverage;
+SELECT * FROM T4_Incident;
+SELECT * FROM T4_Incident_Report;
+SELECT * FROM T4_NOK;
